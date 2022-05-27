79b7b55d121704a2231a0ecbb462913487bd05a4
ClassmateLin <ClassmateLin>
2022-05-27 13:49:53 +0800

    完成无key的m3u8下载功能

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-
 # m3u8-downloader-rs 
 
-rust写的简易m3u8下载器
+Rust练手项目: 一个简易的m3u8下载器
+
+实现原理: 通过`http://xxxxx.com/xxx.m3u8`文件获取ts片段, 下载ts文件合并成一个文件, 目前只支持无key下载。
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{collections::HashMap, path::Path, time::Duration, thread};
 use pbr::{Units, ProgressBar};
+use rand::{distributions::Alphanumeric, Rng};
 use tokio::{fs, io::{AsyncWriteExt}};
 use url::{Url};
 use clap::Parser;
@@ -17,7 +18,7 @@ const M3U8_EXT_KEY_IV:&str = "IV=";
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Name of the person to greet
-    #[clap(short, long, default_value_t=String::from("http://localhost:8000/playlist.m3u8"))]
+    #[clap(short, long, default_value_t=String::from("http://localhost:9999/playlist.m3u8"))]
     url: String,
 }
 
@@ -27,8 +28,8 @@ async fn get_m3u8_content(url:String) -> Result<String, Box<dyn std::error::Erro
 }
 
 // validate m3u8 content
-fn validate_m3u8_content(text:String) -> bool {
-    text.starts_with(M3U8_EXT_HEADER)
+fn validate_m3u8_content(text:String) -> Result<bool, Box<dyn std::error::Error>> {
+    Ok(text.starts_with(M3U8_EXT_HEADER))
 }
 
 /// get ts list
@@ -56,7 +57,7 @@ fn get_ts_list(link:String, text:String) -> Result<Vec::<String>,  Box<dyn std::
 }
 
 // get key info
-fn get_key_info(content: String) -> HashMap<String, String> {
+fn get_key_info(content: String) -> Result<HashMap<String, String>, Box<dyn std::error::Error>> {
     
     let mut m = HashMap::<String, String>::new();
     
@@ -84,17 +85,17 @@ fn get_key_info(content: String) -> HashMap<String, String> {
             
         }
     }
-    m
+    Ok(m)
 }
 
 // get key content
 async fn get_key_content(url: String) ->Result<String, Box<dyn std::error::Error>> {
     let text = reqwest::get(&url).await?.text().await?;
-    Ok("key".to_string())
+    Ok(text)
 }
 
 // download file
-async fn download(link: String) -> Result<(), Box<dyn std::error::Error>> {
+async fn download(link: String) -> Result<String, Box<dyn std::error::Error>> {
     let client = reqwest::Client::builder()
     .build()?;
 
@@ -108,13 +109,12 @@ async fn download(link: String) -> Result<(), Box<dyn std::error::Error>> {
     let respone = client.get(&link).send().await?;
     let content_length = respone.content_length().unwrap(); 
     
-    println!("content length: {}", content_length);
     let mut pb = ProgressBar::new(content_length);
     pb.set_units(Units::Bytes);
     
-    let filename = String::from("./download") + &url.path().to_string();
+    let filename = String::from("./download") + url.path();
   
-    let mut source = client.get(&link).send().await?;
+    let mut source = client.get(&link).header("content-type", "application/octet-stream").send().await?;
  
     let mut dest = fs::OpenOptions::new().create(true).append(true).open(&filename).await?;
     
@@ -123,19 +123,38 @@ async fn download(link: String) -> Result<(), Box<dyn std::error::Error>> {
         pb.add(chunk.len() as u64);
         thread::sleep(Duration::from_millis(10));
     }
-    pb.finish_println(format!("下载成功, {}", filename).as_str());
-    Ok(())
+    pb.finish_println(format!("{}:下载成功!\n", filename).as_str());
+    
+    Ok(filename)
+
 }
 
 // download all
-async fn download_all(ts_list: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
-    println!("总共{}个ts文件...", ts_list.len());
+async fn download_all(ts_list: Vec<String>) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    let mut filename_list:Vec<String> = Vec::new();
 
     for item in ts_list.iter() {
         let link = item.to_string().clone();
-        download(link).await?;
-
+        let filename = download(link).await?;
+        filename_list.push(filename);
     }
+    Ok(filename_list)
+}
+
+// merge file
+async fn merge(filename_list: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+    let mut result = vec![];
+    let result_name = rand::thread_rng()
+    .sample_iter(&Alphanumeric)
+    .take(7)
+    .map(char::from)
+    .collect::<String>() + ".ts";
+    for filename in filename_list.iter() {
+        let content = fs::read(filename).await?;
+        result.extend_from_slice(&content);
+    }
+    fs::write(&result_name, result).await?;
+    println!("文件合并至: ./{}", result_name);
     Ok(())
 }
 
@@ -143,38 +162,35 @@ async fn download_all(ts_list: Vec<String>) -> Result<(), Box<dyn std::error::Er
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
-    let content = match get_m3u8_content(args.url.clone()).await {
-        Ok(text) => text,
-        Err(error) => panic!("Can not get m3u8 info: {:?}", error),
+    let content = get_m3u8_content(args.url.clone()).await?;
+
+    validate_m3u8_content(content.clone())?;
+
+    let ts_list = get_ts_list(args.url.clone(), content.clone())?;
+
+    let key_info = get_key_info(content.clone())?;
+
+    let method = match key_info.get("method") {
+        Some(method) => method.to_string(),
+        None => "".to_string(),
     };
 
+    let _iv = match key_info.get("iv") {
+        Some(method) => method.to_string(),
+        None => "".to_string(),
+    };
 
-    if !validate_m3u8_content(content.clone()){
-        println!("错误的m3u8...");
-    }else {
-        let ts_list = match get_ts_list(args.url.clone(), content.clone()) {
-            Ok(ts) => ts,
-            Err(_) => vec![],
-        };
-        let key_info = get_key_info(content.clone());
-        let method = match key_info.get("method") {
-            Some(method) => method.to_string(),
-            None => "".to_string(),
-        };
-        if method == "" {
-            download_all(ts_list).await?;
-        }else {
-            let _iv = match key_info.get("iv") {
-                Some(method) => method.to_string(),
-                None => "".to_string(),
-            };
-            let _key = match key_info.get("uri") {
-                Some(key) => get_key_content(key.clone()).await?,
-                None => "".to_string(),
-            };
-            download_all(ts_list).await?;
+    let _key = match key_info.get("uri") {
+        Some(key) => get_key_content(key.clone()).await?,
+        None => "".to_string(),
+    };
+
+    if let Ok(filename_list) = download_all(ts_list).await {
+        if method == ""{
+            merge(filename_list).await?;
         }
-        
+    }else {
+        println!("下载失败...");
     }
     
     Ok(())
